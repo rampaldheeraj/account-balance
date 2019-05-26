ae18c7c881c9749339b14bd3ddae6481e86b29d0
Dheeraj Rampal <dheeraj.rampal@reece.com.au>
2019-05-27 08:40:33 +1000

    refactored some code

--- src/main/java/org/rampal/service/RelativeAccountBalanceCheckerService.java
+++ src/main/java/org/rampal/service/RelativeAccountBalanceCheckerService.java
@@ -4,24 +4,27 @@ import org.rampal.data.TransactionData;
 import org.rampal.domain.SubTransaction;
 import org.rampal.request.AccountBalanceCheckRequest;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class RelativeAccountBalanceCheckerService {
 
     public List<SubTransaction> transactionsForAccountBalance(AccountBalanceCheckRequest request) {
-        var allAccountPayments = TransactionData.PAYMENTS.stream()
+        var allAccountTransactions = TransactionData.PAYMENTS.stream()
                 .filter(payment -> payment.getAccountId().equals(request.getAccountId()));
-        var collect = allAccountPayments
+        return allAccountTransactions
                 .filter(onlyPayments())
-                .filter(payment -> payment.getTransactionTime().isAfter(request.getFrom())
-                        && payment.getTransactionTime().isBefore(request.getTo()))
+                .filter(withinDateRange(request.getFrom(), request.getTo()))
                 .collect(Collectors.toList());
-        return collect;
 
 
+    }
+
+    private Predicate<SubTransaction> withinDateRange(LocalDateTime fromDate, LocalDateTime toDate) {
+        return payment -> payment.getTransactionTime().isAfter(fromDate)
+                && payment.getTransactionTime().isBefore(toDate);
     }
 
     private Predicate<SubTransaction> onlyPayments() {
